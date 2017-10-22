ab36e32a5ffbe1ebfdc1218eb8a9c137da61b9e0
Andy Woodruff <andrewwoodruff@Andrews-MacBook-Pro.local>
2017-10-22 18:14:12 -0400

    Initial layer hierarchy showing

--- src/js/events.js
+++ src/js/events.js
@@ -4,6 +4,7 @@ let Dispatch = d3.dispatch('changeyear');
 Dispatch.on('changeyear', function (newYear) {
   year = newYear;
   Map.setYear(newYear);
+  Legend.setYear(newYear);
   Filmstrip.setYear(newYear);
   Search.setYear(newYear);
 });
--- src/js/legend.js
+++ src/js/legend.js
@@ -4,14 +4,46 @@ let Legend = (function($, dispatch) {
 
   let legend = $('#legend');
 
+  let year = 2015;
+
   function init_events () {
     $('.legend-toggle').click(function () {
       legend.toggleClass('collapsed').addClass('subsequent');
     });
   }
 
+  function updateYear (y) {
+    if (y == year) return;
+    year = y;
+    $('.legend-contents').empty();
+    $.getJSON(server + 'layers/' + year, function(json) {
+      _.each(json, function (category, categoryName) {
+        let cat = $('<div>').attr('class', 'legend-category').appendTo('.legend-contents');
+        $('<div>').attr('class', 'category-title').html(categoryName).appendTo(cat);
+        _.each(category, function (obj) { // there's an extra level here
+          _.each(obj, function (group, groupName) {
+            console.log(group)
+            let gr = $('<div>').attr('class', 'legend-group').appendTo(cat);
+            $('<div>').attr('class', 'group-title').html(groupName).appendTo(gr);
+            _.each(group.features, function (feature) {
+              $('<div>').attr('class', 'layer').html(feature).appendTo(gr);
+            });
+          });
+        });
+      });
+    });
+  }
+
   Lg.initialize = function () {
     init_events();
+
+    return Lg;
+  }
+
+  Lg.setYear = function (newYear) {
+    updateYear(newYear);
+
+    return Lg;
   }
 
   return Lg;
