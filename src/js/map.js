cb54c0a64f89a335b060f9435ba2e2fa487f0184
Andy Woodruff <andrewwoodruff@Andrews-MacBook-Pro.local>
2017-10-23 09:07:31 -0400

    Style on layer highlights

--- src/js/map.js
+++ src/js/map.js
@@ -6,7 +6,30 @@ let Map = (function($, dispatch) {
   let map;
   let year = 2015;
   let tileLayer;
-  let highlightLayer;
+  let highlightLayerBottom;
+  let highlightLayerTop;
+  let highlightBottomStyle = {
+    weight: 8,
+    color: '#07407B',
+    opacity: .5
+  };
+  let highlightTopStyle = {
+    weight: 2,
+    color: '#1a1a1a',
+    opacity: 1
+  };
+  let highlightMarkerBottomStyle = {
+    color: '#07407B',
+    weight: 3,
+    fill: false,
+    radius: 3
+  }
+  let highlightMarkerTopStyle = {
+    color: '#1a1a1a',
+    weight: 1,
+    fill: false,
+    radius: 3
+  }
 
   M.initialize = function (container) {
     map = L.map(container, {zoomControl: false}).setView([29.717, -95.402], 16);
@@ -19,16 +42,24 @@ let Map = (function($, dispatch) {
     if (newYear == year) return;
     year = newYear;
     tileLayer.setUrl(tileserver + year + '/all/{z}/{x}/{y}.png');
+    M.removeHighlight();
     return M;
   }
 
   M.highlightFeature = function (geojson) {
     M.removeHighlight();
-    highlightLayer = L.geoJson(geojson).addTo(map);
+    highlightLayerBottom = L.geoJson(geojson, {
+      style: () => highlightBottomStyle,
+      pointToLayer: (pt, latlng) => L.circleMarker(latlng, highlightMarkerBottomStyle)
+    }).addTo(map);
+    highlightLayerTop = L.geoJson(geojson, {
+      style: () => highlightTopStyle,
+      pointToLayer: (pt, latlng) => L.circleMarker(latlng, highlightMarkerTopStyle)
+    }).addTo(map);
   }
 
   M.removeHighlight = function () {
-    if (highlightLayer && map.hasLayer(highlightLayer)) map.removeLayer(highlightLayer);
+    if (highlightLayerBottom && map.hasLayer(highlightLayerBottom)) map.removeLayer(highlightLayerBottom).removeLayer(highlightLayerTop);
   }
 
   return M;
