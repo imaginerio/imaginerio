51364f97af90456cc4487be8d19fcd32b56d79b3
Andy Woodruff <awoodruff@gmail.com>
2017-12-20 14:19:07 -0500

    Update era descriptions
    fix #66

--- src/js/eras.js
+++ src/js/eras.js
@@ -2,49 +2,49 @@ const eras = [
  {
    name: "Planning / Construction / Inauguration",
    dates: [1898,1912],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "This inaugural and eventful era saw the chartering of the Rice Institute, the death of the founder, the hiring of the first president, and the construction of the first campus buildings, culminating in the 1912 Formal Opening.",
    key_buildings: "Administration, Power House, Residential Group",
    other_buildings: "Rice Field"
  },
  {
    name: "Building an Institute",
    dates: [1913,1938],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "Although campus expansion was hampered by World War I and the Great Depression, several new academic and residential buildings sprang up as enrollment grew and the faculty required first-rate research space.",
    key_buildings: "Physics, Chemistry, East & West Halls, Cohen House, Willy's Statue",
    other_buildings: "Autry House/Palmer Memorial"
  },
  {
    name: "The Post-war Plan",
    dates: [1939,1949],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "The first wave of post-World war II war growth began with the construction of a new classroom building, new laboratory space, and a badly needed library that for the first time brought the Institute’s book collection under one roof.",
    key_buildings: "Anderson Hall, Fondren Library, Ambercrombie",
    other_buildings: "NROTC, President's House, Wiess Hall"
  },
  {
    name: "Science Boom and Residential Colleges",
    dates: [1950,1968],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "A second wave of expansion accompanied a significant increase in enrollment and a new focus on federally funded scientific and technical research. In addition to the necessary new laboratory space, a student center, a new gymnasium, a theater, and football stadium were built.",
    key_buildings: "Stadium/Fieldhouse, Biology-Geoscience-Space Science-Math, Bonner Lab, Residential College System, RMC",
    other_buildings: "Ryon Labs, Rayzor, Hamman Hall, FE&P, Allen Center"
  },
  {
    name: "Growing a University",
    dates: [1969,1989],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "An era of consolidation, these years saw steady and balanced growth in engineering and the humanities as well as new student housing. A home for the new school of business was another addition to campus.",
    key_buildings: "Sewall Hall, Sid Rich College, Herring Hall",
    other_buildings: "Ley Student Center, Cox Mech, Rice Media Center"
  },
  {
    name: "Continuing a Tradition",
    dates: [1990,2000],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "This era was characterized by a sustained expansion in the areas of humanities, social sciences, and the arts. New laboratory and teaching spaces were provided for the growing fields of computer science and biochemistry.",
    key_buildings: "George R. Brown, Shepherd School, Duncan Hall, Baker Institute, Humanities Building",
    other_buildings: "Reckling Park"
  },
  {
    name: "Expanding the Colleges and New Architecture",
    dates: [2001,2017],
-   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna wirl aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisinuli.",
+   description: "With the addition of such important buildings as Brockman Hall for Physics, Gibbs Recreation and Wellness Center, Brochstein Pavilion, Moody Center for the Arts, new colleges and an opera house, a renewed and transformative architectural campaign marks this most recent era.",
    key_buildings: "New Colleges, Rec Center, Brockman Physics, Brochstein Pavilion, Moody Center",
    other_buildings: "Turrell Skyspace, BRC"
  }
