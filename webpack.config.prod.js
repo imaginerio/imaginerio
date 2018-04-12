00fcfc350cbe3b6c28a158800cd16225f28ad4e8
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-04-12 14:45:10 -0400

    Set up webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/entry.js'],
+  output: {
+    path: path.join(__dirname),
+    filename: './src/js_build/bundle.js',
+  },
+  watch: false,
+  mode: 'production',
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   enforce: 'pre',
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: true,
+      //   },
+      // },
+      {
+        test: /\.js$/, 
+        exclude: [/node_modules/], 
+        loader: 'babel-loader',
+        query: {
+          presets: ['env'],
+        },
+      },
+    ],
+  },
+};
