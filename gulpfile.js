c7ea1606a328d4c6acb18ecc6a416d515d950816
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-04-12 10:49:25 -0400

    Uglify in production mode

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const gulpif = require('gulp-if');
+const del = require('del');
+
+const env = process.env.NODE_ENV || 'development';
+
+gulp.task('clean', () => del(['src/js_build/*']));
 
 gulp.task('es6', () =>
   gulp.src('src/js/*.js')
@@ -13,10 +20,12 @@ gulp.task('es6', () =>
       console.log('error', e);
       this.emit('end');
     })
+    .pipe(gulp.dest('src/js_build'))
+    .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest('src/js_build')));
 
 gulp.task('watch', () => {
   gulp.watch('src/js/*.js', ['es6']);
 });
 
-gulp.task('default', ['es6', 'watch']);
+gulp.task('default', ['clean', 'es6', 'watch']);
