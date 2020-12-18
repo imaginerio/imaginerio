d8031a91bc922fbe93785dac581651ae3c58cb7b
David Heyman <dave@axismaps.com>
2020-12-18 10:19:19 +0000

    Fixed photos

--- src/js/Photo.js
+++ src/js/Photo.js
@@ -15,41 +15,34 @@ let Photo = function (data, thumbUrl) {
   let request;
 
   function getMetadata() {
-    window.fetch(`https://dmi.rice.edu:8443/rest/handle/${P.data.id}`, { credentials : 'include' })
-      .then(res => res.json())
-      .then((meta) => {
-        window.fetch(`https://dmi.rice.edu:8443/rest/items/${meta.uuid}/bitstreams`, { credentials: 'include' })
-          .then(res => res.text())
-          .then((text) => {
-            const json = JSON.parse(text);
-            if (json.length > 1) {
-              P.metadata = json.find(i => i.mimeType.match(/image/));
-            } else {
-              [P.metadata] = json;
-            }
-            tempImages.forEach((img) => {
-              const image = new Image();
-                image.onload = function () {
-                  img.div.empty().css('background-image', `url(${getUrl()})`);
-                  P.metadata.width = this.width;
-                  P.metadata.height = this.height;
-                  img.div.empty().css('background-image', `url(${getUrl(img.size)})`);
-                  if (img.setDimensions) {
-                    const s = P.getScaled(img.size);
-                    img.div.css('width', `${s[0]}px`).css('height', `${s[1]}px`);
-                  }
-                }
-                image.src = getUrl();
-            });
-            P.href = `https://library.artstor.org/#/asset/${P.data.repository}`;
-          });
-      });
+    tempImages.forEach((img) => {
+      const image = new Image();
+        image.onload = function () {
+          img.div.empty().css('background-image', `url(${getUrl()})`);
+          P.metadata.width = this.width;
+          P.metadata.height = this.height;
+          img.div.empty().css('background-image', `url(${getUrl(img.size)})`);
+          if (img.setDimensions) {
+            const s = P.getScaled(img.size);
+            img.div.css('width', `${s[0]}px`).css('height', `${s[1]}px`);
+          }
+        }
+        image.src = getUrl();
+    });
+    P.href = `https://library.artstor.org/#/asset/${P.data.repository}`;
   }
 
   getMetadata();
 
-  function getUrl() {
-    return `https://dmi.rice.edu:8443${P.metadata.retrieveLink}`;
+  function getUrl(size) {
+    let imgSize = 'thumb';
+    console.log(size);
+    if (size && (size[0] > 400 || size[1] > 400 )){
+      imgSize = 'full';
+    } else if (size && (size[0] >= 150 || size[1] >= 150 )){
+      imgSize = 'medium';
+    }
+    return `http://instituterice-images.s3-website-us-east-1.amazonaws.com/${P.data.file}/${imgSize}.jpg`;
   }
 
   P.getImage = function (size, setDimensionsOnLoad) {
